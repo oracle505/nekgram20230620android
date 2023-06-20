bf3de798486c8555df127d24a0d5998fb35fadfc
oracle505 <you@example.com>
2023-06-20 03:33:10 -0700

    add Extra.java  APP_ID  APP_HASH

---
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/Extra.java
@@ -0,0 +1,38 @@
+package tw.nekomimi.nekogram;
+
+import org.telegram.messenger.Utilities;
+
+import tw.nekomimi.nekogram.helpers.MessageHelper;
+
+public class Extra {
+    // See UpdateHelper.java
+    public static final int UPDATE_CHANNEL_ID = 0;
+    public static final String UPDATE_CHANNEL = "";
+
+    // https://core.telegram.org/api/obtaining_api_id
+    public static final int APP_ID = 0;
+    public static final String APP_HASH = "";
+
+    // https://developers.google.com/identity/sms-retriever/verify#computing_your_apps_hash_string
+    public static final String SMS_HASH = "";
+
+    public static final String PLAYSTORE_APP_URL = "";
+
+    // https://appcenter.ms/
+    public static final String APPCENTER_SECRET = "";
+
+    public static final String USER_INFO_BOT = "";
+    public static final long USER_INFO_BOT_ID = 0;
+
+    public static String WS_USER_AGENT = "";
+    public static String WS_CONN_HASH = "";
+    public static String WS_DEFAULT_DOMAIN = "";
+
+    public static long getOwnerFromStickerSetId(long id) {
+        throw new RuntimeException("Apparently not possible");
+    }
+
+    public static void getRegDate(long userId, Utilities.Callback<MessageHelper.RegDate> callback) {
+        throw new RuntimeException("Apparently not possible");
+    }
+}
